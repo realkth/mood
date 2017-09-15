df9ad196ec3e06d5279660112c2d53175874cc8a
gose <gom0367@gmail.com>
2017-09-15 13:59:11 +0900

    수정

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="container">
+      <div class="grid">
+        <div class="col">
+          <h1>
+            <img src="./assets/l-mood.svg" alt="mood 로고" width="200">
+          </h1>
+        </div>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -11,13 +19,28 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "~style";
+
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+body {
+  background: #c3c194; /* For browsers that do not support gradients */
+  background: -webkit-linear-gradient(#c3c194 10%, #6f8b78 60%,#353e45 100%); /* For Safari 5.1 to 6.0 */
+  background: -o-linear-gradient(#c3c194 10%, #6f8b78 60%,#353e45 100%); /* For Opera 11.1 to 12.0 */
+  background: -moz-linear-gradient(#c3c194 10%, #6f8b78 60%,#353e45 100%); /* For Firefox 3.6 to 15 */
+  background: linear-gradient(#c3c194 10%, #6f8b78 60%,#353e45 100%); /* Standard syntax */
+  height: 100vh;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  padding-top: 60px;
+
 }
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,60 +1,86 @@
 <template>
-  <div class="login">
-    <h3>Sign In</h3>
-    <input type="text" v-model="email" placeholder="Email"><br>
-    <input type="password" v-model="password" placeholder="Password"><br>
-    <button v-on:click="signIn">Connection</button>
-    <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link></p>
+  <div class="login container">
+    <div class="grid">
+      <div class="box col col-d-6 col-d-offset-3 col-m-4">
+        <div class="col">
+          <h3>로그인</h3>
+        </div>
+        <div class="col">
+          <input type="text" v-model="email" placeholder="Email">
+        </div>
+        <div class="col">
+          <input type="password" v-model="password" placeholder="Password">
+        </div>
+        <div class="col">
+          <button v-on:click="signIn">Connection</button>
+        </div>
+        <div class="col">
+          <p>You don't have an account ? You can
+            <router-link to="/sign-up">create one</router-link>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import firebase from 'firebase'
+import firebase from 'firebase'
 
-  export default {
-    name: 'login',
-    data: function () {
-      return {
-        email: '',
-        password: ''
-      }
-    },
-    methods: {
-      signIn: function () {
-        firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
-          (user) => {
-            this.$router.replace('hello')
-          },
-          (err) => {
-            console.log(error)
-            alert('Oops. ' + err.message)
-          }
-        );
-      }
+export default {
+  name: 'login',
+  data: function() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signIn: function() {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+        (user) => {
+          this.$router.replace('hello')
+        },
+        (err) => {
+          console.log(error)
+          alert('Oops. ' + err.message)
+        }
+      );
     }
   }
+}
 </script>
 
-<style scoped>  /* "scoped" attribute limit the CSS to this component only */
-  .login {
-    margin-top: 40px;
+<style lang="scss" scoped>
+  /* "scoped" attribute limit the CSS to this component only */
+
+  @import "~style";
+  div {
+    text-align: center
   }
-  input {
-    margin: 10px 0;
-    width: 20%;
-    padding: 15px;
-  }
-  button {
-    margin-top: 20px;
-    width: 10%;
-    cursor: pointer;
-  }
-  p {
-    margin-top: 40px;
-    font-size: 13px;
-  }
-  p a {
-    text-decoration: underline;
-    cursor: pointer;
-  }
+  .box {
+    @extend %box-style; // width: 500px;
+    // height: 500px;
+    display: block;
+  } // .login {
+  //   margin-top: 40px;
+  // }
+  // input {
+  //   margin: 10px 0;
+  //   width: 20%;
+  //   padding: 15px;
+  // }
+  // button {
+  //   margin-top: 20px;
+  //   width: 10%;
+  //   cursor: pointer;
+  // }
+  // p {
+  //   margin-top: 40px;
+  //   font-size: 13px;
+  // }
+  // p a {
+  //   text-decoration: underline;
+  //   cursor: pointer;
+  // }
 </style>

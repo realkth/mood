5a6b6580d80b15e02f6f3795637ad2b62e70e523
realkth <miamirocky@gmail.com>
2017-09-21 17:30:43 +0900

    writemodal 코드 정리

--- src/components/WriteModal.vue
+++ src/components/WriteModal.vue
@@ -7,14 +7,6 @@
           <h3> {{ nowTime(new Date()) }} </h3>
         </header>
         <section class="modal-body">
-          <!-- <div class="emoji-wrapper">
-                  <img class="haha" src="../assets/emoji-haha.svg" alt="뿌듯" height="40px">
-                  <img class="happy" src="../assets/emoji-happy.svg" alt="행복" height="40px">
-                  <img class="soso" src="../assets/emoji-soso.svg" alt="그저그래" height="40px">
-                  <img class="sad" src="../assets/emoji-sad.svg" alt="슬픔" height="40px">
-                  <img class="surprised" src="../assets/emoji-surprised.svg" alt="놀람" height="40px">
-                  <img class="angry" src="../assets/emoji-angry.svg" alt="화남" height="40px">
-                  </div> -->
           <form class="emoji-wrapper">
             <input type="radio" id="haha" name="emotion">
             <label class="haha" for="haha"></label>
@@ -58,7 +50,7 @@ export default {
       default: false,
     },
   },
-  created () {
+  created() {
     this.getUserInfo()
   },
   data() {
@@ -67,7 +59,7 @@ export default {
       write: {
         content: ''
       },
-      name:''
+      name: ''
     }
   },
   methods: {
@@ -109,7 +101,7 @@ export default {
       }
       this.name = name
     },
-    placeholder: function(){
+    placeholder: function() {
       return this.name + "님, 오늘 하루는 어떠셨나요?"
     }
   }

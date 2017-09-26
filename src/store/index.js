3e31f74a024999accad3d3aeda776e556fcfcb73
realkth <miamirocky@gmail.com>
2017-09-26 20:42:04 +0900

    달력 기능 구현 완료

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import SignUp from './modules/SignUp'
 import FirstSetting from './modules/FirstSetting'
 import SecondSetting from './modules/SecondSetting'
 import MySettingModal from './modules/MySettingModal'
+import Calendar from './modules/Calendar'
 import * as firebase from 'firebase'
 import router from './../router/'
 import axios from 'axios'
@@ -15,7 +16,7 @@ export const store = new Vuex.Store({
   // strict: process.env.NODE_ENV !== 'production',
   // 저장소를 모듈화하면 필요. 일단은 그냥 모듈화하지 말고 하나로 만들어보자(나중에 혹시나 필요하면 바꾸면 됨)
   modules: {
-    SignIn, SignUp, FirstSetting, SecondSetting, MySettingModal
+    SignIn, SignUp, FirstSetting, SecondSetting, MySettingModal, Calendar
   },
   state: {
 

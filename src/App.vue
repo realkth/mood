715b53731d2c195a288a2b94db477c546af972df
realkth <miamirocky@gmail.com>
2017-10-16 16:24:41 +0900

    브라우저 체크 메서드 추가.

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,47 @@ export default {
   computed: {
     ...mapGetters(['isToastMessage']),
   },
+  created () {
+    this.checkBrowser()
+  },
+  methods: {
+    checkBrowser:() => {
+      var agent = navigator.userAgent.toLowerCase(),
+        name = navigator.appName,
+        browser;
+
+      // MS 계열 브라우저를 구분하기 위함.
+      if (name === 'Microsoft Internet Explorer' || agent.indexOf('trident') > -1 || agent.indexOf('edge/') > -1) {
+        browser = 'ie';
+        if (name === 'Microsoft Internet Explorer') { // IE old version (IE 10 or Lower)
+          agent = /msie ([0-9]{1,}[\.0-9]{0,})/.exec(agent);
+          browser += parseInt(agent[1]);
+        } else { // IE 11+
+          if (agent.indexOf('trident') > -1) { // IE 11 
+            browser += 11;
+          } else if (agent.indexOf('edge/') > -1) { // Edge
+            browser = 'edge';
+          }
+        }
+      } else if (agent.indexOf('safari') > -1) { // Chrome or Safari
+        if (agent.indexOf('opr') > -1) { // Opera
+          browser = 'opera';
+        } else if (agent.indexOf('chrome') > -1) { // Chrome
+          browser = 'chrome';
+        } else { // Safari
+          browser = 'safari';
+        }
+      } else if (agent.indexOf('firefox') > -1) { // Firefox
+        browser = 'firefox';
+      }
+
+      document.getElementsByTagName('html')[0].className = browser;
+      if (browser.indexOf("Microsoft Internet Explorer") > -1 || agent.indexOf('trident') > -1){
+      alert('브라우저 바꾸세여..')
+      }
+    },
+    
+  }
 }
 </script>
 
@@ -32,8 +73,7 @@ body {
   /* For Firefox 3.6 to 15 */
   background: linear-gradient(#c3c194 10%, #6f8b78 60%, #353e45 100%);
   /* Standard syntax */
-  min-height: 100vh;
-  // max-height: 100%;
+  min-height: 100vh; // max-height: 100%;
 }
 
 // @media screen and (min-width: 0px) and (max-width: 767px) {
@@ -42,7 +82,6 @@ body {
 //   //   max-height: 160vh;
 //   // }
 // }
-
 // @media screen and (min-width: 768px) and (max-width: 1200px) {
 //   body {
 //     min-height: 100vh;
@@ -56,7 +95,6 @@ body {
 //     height: 100%;
 //   }
 // }
-
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
